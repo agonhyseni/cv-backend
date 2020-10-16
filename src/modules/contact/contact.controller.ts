995e323f106f1e4ac1e5d6e7a1918b215839e563
Agon Hyseni <hyseniagon@gmail.com>
2020-10-16 14:00:41 +0200

    Created contact controller

---
+++ src/modules/contact/contact.controller.ts
@@ -0,0 +1,27 @@
+import { Controller, Get, Post, Body, ValidationPipe } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Contact } from 'src/entities/Contact.entity';
+import { ContactService } from './contact.service';
+import { SendEmailDto } from './dto/send-email-dto';
+
+@ApiTags('Contact')
+@Controller()
+export class ContactController {
+  constructor(private contactService: ContactService) {}
+
+  @ApiOperation({ summary: 'Get contact info' })
+  @Get('contact')
+  async getContacts(): Promise<{ contact: Contact }> {
+    const contact = await this.contactService.getMe();
+    return { contact };
+  }
+
+  @ApiOperation({ summary: 'Contact me' })
+  @Post('contact/me')
+  async createContacts(
+    @Body(ValidationPipe) sendEmailDto: SendEmailDto,
+  ): Promise<void> {
+    const response = await this.contactService.sendEmail(sendEmailDto);
+    console.log(response);
+  }
+}
