f6d82a1540bfff320487b6a4cb4159be5d071981
agonhyseni <hyseniagon@gmail.com>
2020-11-16 23:21:57 +0100

    Commented cors and ratelimit

--- src/main.ts
+++ src/main.ts
@@ -5,23 +5,23 @@ dotenv.config();
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 // import * as helmet from 'helmet';
-import * as rateLimit from 'express-rate-limit';
+// import * as rateLimit from 'express-rate-limit';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // app.use(helmet());
-  app.enableCors({
-    maxAge: 3600,
-    credentials: true,
-    origin: process.env.CORS_URL,
-  });
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 100, // limit each IP to 100 requests per windowMs
-    }),
-  );
+  // app.enableCors({
+  //   maxAge: 3600,
+  //   credentials: true,
+  //   origin: process.env.CORS_URL,
+  // });
+  // app.use(
+  //   rateLimit({
+  //     windowMs: 15 * 60 * 1000, // 15 minutes
+  //     max: 100, // limit each IP to 100 requests per windowMs
+  //   }),
+  // );
 
   const options = new DocumentBuilder()
     .setTitle(`${process.env.FIRSTNAME} ${process.env.LASTNAME} - CV`)
